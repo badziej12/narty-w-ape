ecd1fcd5ee25d4ee0690275fd5681bd8f7d96cca
badziej12 <badziej12@gmail.com>
2023-08-30 23:09:10 +0200

    Update

---
+++ static/js/navmobile.js
@@ -0,0 +1,4 @@
+$('.header__navmobile__con__burgercon').on('click', function () {
+    $('.header__navmobile__menucon').toggleClass('active')
+    // $('.header__navmobile__con__burgercon__burger span').anima
+})
