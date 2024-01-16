4c05c76ca6dfad2d058fdf85c22f2b8abaff0bd1
badziej12 <badziej12@gmail.com>
2024-01-16 16:41:54 +0100

    Update

---
+++ backup/public_html/static/js/navmobile.js
@@ -0,0 +1,4 @@
+$('.header__navmobile__con__burgercon, .header__navmobile__closebg').on('click', function () {
+    $('.header__navmobile__menucon, .header__navmobile__closebg, .header__navmobile__con__burgercon__burger span').toggleClass('active');
+    console.log('xos');
+})
